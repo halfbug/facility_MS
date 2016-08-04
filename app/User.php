96b06e24c41f9f224da6b691dc4ecbce5886d4c9
Sadaf Siddiqui <sadaf_cu@hotmail.com>
2016-08-04 13:25:18 +0500

    test here

---
+++ app/User.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name','last_name','username', 'email', 'password','is_approved', 'approved_by', 'approved_date'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
